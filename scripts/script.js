e3a832397c51b63121a905fcf37fc4177223f069
Sam Legros <email@samlegros.com>
2018-10-24 13:56:48 -0400

    Add back to top button

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
+// smooth-scroll
 var scroll = new SmoothScroll('a[href*="#"]', {speed: 1000});
 
+// typed.js
 var options = {
     strings: ["I'm a web developer.", "^250 I'm an avid reader.", "^250 I'm a big sci-fi fan.", "^250 I'm probably thinking of what to do for dinner right now.", "^250 I'm looking for new opportunities!"],
     typeSpeed: 100,
@@ -11,6 +13,20 @@ var options = {
 
 var typed = new Typed("#home-typed", options);
 
+// back to top button
+var pixelsFromTop = 500;
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > pixelsFromTop || document.documentElement.scrollTop > pixelsFromTop) {
+        document.getElementById("back-to-top-button").style.display = "block";
+    } else {
+        document.getElementById("back-to-top-button").style.display = "none";
+    }
+}
+
+// general page
 $('.btn-alt').on('click', function() {
     alert("Source code for projects coming soon!");
 });
