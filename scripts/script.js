14474a454ac03972b655157a7889d2c4024f5fe4
Sam Legros <email@samlegros.com>
2019-02-24 12:41:10 -0500

    Remove new opportunities from typed.js array

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ var scroll = new SmoothScroll('a[href*="#"]', {speed: 1000});
 
 // typed.js
 var options = {
-    strings: ["I'm a web developer.", "^250 I'm an avid reader.", "^250 I'm a big sci-fi fan.", "^250 I'm probably thinking of what to do for dinner right now.", "^250 I'm looking for new opportunities!"],
+    strings: ["I'm a web developer.", "^250 I'm an avid reader.", "^250 I'm a big sci-fi fan.", "^250 I'm probably thinking of what to do for dinner right now."],
     typeSpeed: 100,
     backSpeed: 50,
     smartBackspace: true,
