4dd2a431e874eaa87f1cb55658f7113ecbfa9620
Sam Legros <sam.legros@curatemobile.com>
2019-06-03 13:38:43 -0400

    Change name from Legros to Sverko
    Add email back to website.
    Refactor script.

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,33 @@
 // smooth-scroll
-var scroll = new SmoothScroll('a[href*="#"]', {speed: 1000});
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000
+});
 
 // typed.js
 var options = {
-    strings: ["I'm a web developer.", "^250 I'm an avid reader.", "^250 I'm a big sci-fi fan.", "^250 I'm probably thinking of what to do for dinner right now."],
-    typeSpeed: 100,
-    backSpeed: 50,
-    smartBackspace: true,
-    loop: true,
-    loopCount: Infinity,
-}
+	strings: ["I'm a web developer.", "^250 I'm an avid reader.", "^250 I'm a big sci-fi fan.", "^250 I'm probably thinking of what to do for dinner right now."],
+	typeSpeed: 100,
+	backSpeed: 50,
+	smartBackspace: true,
+	loop: true,
+	loopCount: Infinity,
+};
 
 var typed = new Typed("#home-typed", options);
 
 // back to top button
 var pixelsFromTop = 500;
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+	scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > pixelsFromTop || document.documentElement.scrollTop > pixelsFromTop) {
-        document.getElementById("back-to-top-button").style.display = "block";
-    } else {
-        document.getElementById("back-to-top-button").style.display = "none";
-    }
+	if (document.body.scrollTop > pixelsFromTop || document.documentElement.scrollTop > pixelsFromTop) {
+		document.getElementById("back-to-top-button").style.display = "block";
+	} else {
+		document.getElementById("back-to-top-button").style.display = "none";
+	}
 }
 
 // resize font
@@ -31,33 +35,33 @@ var resizeFontSection = document.getElementById('section-settings');
 var resizeButton = resizeFontSection.getElementsByTagName('button');
 
 for (var i = 0; i < resizeButton.length; i++) {
-    resizeButton[i].addEventListener('click', resizeFont.bind(this, (i*50)+100));
+	resizeButton[i].addEventListener('click', resizeFont.bind(this, (i * 50) + 100));
 }
 
 function resizeFont(size) {
-    var navBar = document.getElementsByClassName('navbar');
-    if (size == '200') {
-        $('#css-font-size-1-5').remove();
-        var fontSizeCSS = document.createElement('link');
-        fontSizeCSS.setAttribute('rel', 'stylesheet');
-        fontSizeCSS.setAttribute('type', 'text/css');
-        fontSizeCSS.setAttribute('href', 'styles/font-size-2.css');
-        fontSizeCSS.setAttribute('id', 'css-font-size-2');
-        document.getElementsByTagName('head')[0].appendChild(fontSizeCSS);
-        navBar[0].classList.remove('navbar-expand-md');
-    } else if (size == '150') {
-        $('#css-font-size-2').remove();
-        var fontSizeCSS = document.createElement('link');
-        fontSizeCSS.setAttribute('rel', 'stylesheet');
-        fontSizeCSS.setAttribute('type', 'text/css');
-        fontSizeCSS.setAttribute('href', 'styles/font-size-1-5.css');
-        fontSizeCSS.setAttribute('id', 'css-font-size-1-5');
-        document.getElementsByTagName('head')[0].appendChild(fontSizeCSS);
-        navBar[0].classList.remove('navbar-expand-md');
-    } else {
-        $('#css-font-size-1-5').remove();
-        $('#css-font-size-2').remove();
-        navBar[0].classList.add('navbar-expand-md');
-        var cssSheets = document.getElementsByTagName('link');
-    }
+	var navBar = document.getElementsByClassName('navbar');
+	if (size == '200') {
+		$('#css-font-size-1-5').remove();
+		var fontSizeCSS = document.createElement('link');
+		fontSizeCSS.setAttribute('rel', 'stylesheet');
+		fontSizeCSS.setAttribute('type', 'text/css');
+		fontSizeCSS.setAttribute('href', 'styles/font-size-2.css');
+		fontSizeCSS.setAttribute('id', 'css-font-size-2');
+		document.getElementsByTagName('head')[0].appendChild(fontSizeCSS);
+		navBar[0].classList.remove('navbar-expand-md');
+	} else if (size == '150') {
+		$('#css-font-size-2').remove();
+		var fontSizeCSS = document.createElement('link');
+		fontSizeCSS.setAttribute('rel', 'stylesheet');
+		fontSizeCSS.setAttribute('type', 'text/css');
+		fontSizeCSS.setAttribute('href', 'styles/font-size-1-5.css');
+		fontSizeCSS.setAttribute('id', 'css-font-size-1-5');
+		document.getElementsByTagName('head')[0].appendChild(fontSizeCSS);
+		navBar[0].classList.remove('navbar-expand-md');
+	} else {
+		$('#css-font-size-1-5').remove();
+		$('#css-font-size-2').remove();
+		navBar[0].classList.add('navbar-expand-md');
+		var cssSheets = document.getElementsByTagName('link');
+	}
 }
